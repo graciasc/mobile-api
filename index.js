aec273b27f3fffd88b6996f0b0c9adf1204e2110
Gracias <gracias.claude@outlook>
2020-12-08 19:21:44 -0700

    update route

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const port = process.env.PORT || 3000;
 const routes = require('./routes')
 
 app.use(express.json())
-// app.use('/', (req, res) => {
-// res.send('home')
-// })
+app.use('/', (req, res) => {
+res.send('home')
+})
 
 app.listen(port, () => {
   console.log("Running")
 });
+
+
 
 app = routes.register(app)
