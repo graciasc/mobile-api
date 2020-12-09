81d97e55ee3c496bc5c0b7196a909194ed038b0a
Gracias <gracias.claude@outlook>
2020-12-09 08:44:15 -0700

    fix routes for list

--- list/listController.js
+++ list/listController.js
@@ -4,13 +4,13 @@ module.exports = {
 
   async create(req, res) {
     res.send('ToDo Creation')
-    let sql = `INSERT INTO Items (name, description, priority) VALUES (${req.body.name},${req.body.description},${req.body.priority});`
+    let sql = `INSERT INTO Items (name, description, priority) VALUES ("${req.body.name}","${req.body.description}","${req.body.priority})";`
     let result = await db.create(sql)
     console.log(result)
   },
   async delete(req, res) {
     res.send('ToDo delete')
-    let sql = `DELETE FROM Items WHERE id = ${req.body.id};`
+    let sql = `DELETE FROM Items WHERE id = "${req.body.id}";`
     let result = await db.remove(sql)
     console.log(result)
   },
