1af15cb4c86d2477af141d2ed45dfe8151fc80a8
Gracias <gracias.claude@outlook>
2020-12-08 20:28:38 -0700

    remove home route

--- contact/contactController.js
+++ contact/contactController.js
@@ -3,7 +3,6 @@ let db = require('../database')
 module.exports = {
 
   async create(req, res) {
-    res.send('Contact Creation')
     let sql = `INSERT INTO Contacts (name, age, realtedHow) VALUES (${req.body.name},${req.body.age},${req.body.relatedHow});`
     let result = await db.create(sql)
     console.log(result)
@@ -15,11 +14,10 @@ module.exports = {
     console.log(result)
   },
   async list(req, res) {
-    res.send('Contact list')
+ 
     let sql = `SELECT * FROM Contacts;`
     let result = await db.list(sql)
-    console.log(result)
-    // res.send(result)
+    res.send(result)
   }
 
 }
--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
-app.use('/', (req, res) => {
-res.send('home')
-})
+
 
 app.listen(port, () => {
   console.log("Running")
